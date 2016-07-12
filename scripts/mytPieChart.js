40986eb819cad882f8e3661e918a1f58936a5f54
anteks <ante@keepersolutions.com>
2016-07-12 13:13:00 +0200

    Recent changes: - added color ranges for up to 100 slices - colors vary in base color and change in light % in groups of 10 - added container and id params - added function 'generateTestData' that takes 1 argument (n) and returns test dataset with n slices

--- scripts/mytPieChart.js
+++ scripts/mytPieChart.js
@@ -52,6 +52,22 @@
       data: this.data,
       container: this.container,
       id: this.id,
+      chartExists: false,
+
+      color: d3.scale.ordinal()
+        .range([
+          // Blue,     Green,     Yellow,     Purple,    Red,      Brown,     Pink,      Orange,    L.Blue,   L.Green
+          "#ccccff", "#ccffcc", "#ffffcc", "#f2ccff", "#ffcccc", "#f2e6d9", "#ffccff", "#ffe0cc", "#ccffff", "#e6ffcc", // 1st layer at 90%
+          "#b3b3ff", "#b3ffb3", "#ffffb3", "#ecb3ff", "#ffb3b3", "#ecd9c6", "#ffb3ff", "#ffd1b3", "#b3ffff", "#d9ffb3", // 2nd layer at 85%
+          "#9999ff", "#99ff99", "#ffff99", "#e699ff", "#ff9999", "#e6ccb3", "#ff99ff", "#ffc299", "#99ffff", "#ccff99", // 3rd layer at 80%
+          "#8080ff", "#80ff80", "#ffff80", "#df80ff", "#ff8080", "#dfbf9f", "#ff80ff", "#ffb380", "#80ffff", "#bfff80", // 4th layer at 75%
+          "#6666ff", "#66ff66", "#ffff66", "#d966ff", "#ff6666", "#d9b38c", "#ff66ff", "#ffa366", "#66ffff", "#b3ff66", // 5th layer at 70%
+          "#4d4dff", "#4dff4d", "#ffff4d", "#d24dff", "#ff4d4d", "#d2a679", "#ff4dff", "#ff944d", "#4dffff", "#a6ff4d", // 6th layer at 65%
+          "#3333ff", "#33ff33", "#ffff33", "#cc33ff", "#ff3333", "#cc9966", "#ff33ff", "#ff8533", "#33ffff", "#99ff33", // 7th layer at 60%
+          "#1a1aff", "#1aff1a", "#ffff1a", "#c61aff", "#ff1a1a", "#c68c53", "#ff1aff", "#ff751a", "#1affff", "#8cff1a", // 8th layer at 55%
+          "#0000ff", "#00ff00", "#ffff00", "#bf00ff", "#ff0000", "#bf8040", "#ff00ff", "#ff6600", "#00ffff", "#80ff00", // 9th layer at 50%
+          "#0000e6", "#00e600", "#e6e600", "#ac00e6", "#e60000", "#ac7339", "#e600e6", "#e65c00", "#00e6e6", "#73e600", // 10th layer at 45%
+        ]),
 
       getWidth: function(){
         return width;
@@ -77,14 +93,24 @@
         data = d;
       },
 
-      draw: function(){
+      redraw: function(){
         // Set radius to half of the smaller side
         // to avoid 'out-of-bounds' issue
         var radius = Math.min(width,height) / 2;
+      },
 
-        // Color range
-        var color = d3.scale.ordinal()
-          .range(["#ff6666", "#99ddff", "#ffff66", "#66ff66", "#ffb366", "#c2c2a3", "#ff66d9", "#b3ff66", "#ffd966", "#6666ff"]);
+      draw: function(){
+
+        if(this.chartExists){
+          redraw();
+          return;
+        }
+
+        var color = this.color;
+
+        // Set radius to half of the smaller side
+        // to avoid 'out-of-bounds' issue
+        var radius = Math.min(width,height) / 2;
 
         var arc = d3.svg.arc()
           .outerRadius(radius - 10)
@@ -114,24 +140,38 @@
             console.log(d.data.name + " - " + d.data.value);
           })
           .on("mouseout", function(d){
-            d3.select(this).select("path").attr("r", 5.5).style("opacity", "0.5");
+            d3.select(this).select("path").attr("r", 5.5).style("opacity", "0.7");
           });
 
         g.append("path")
           .attr("d", arc)
           .style("fill", function(d) { return color(d.data.name); })
-          .style("opacity", "0.5");
+          .style("opacity", "0.7");
 
         g.append("text")
           .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
           .attr("dy", ".35em")
           .style("opacity", "1")
           .text(function(d) { return d.data.name; });
+
+        chartExists = true;
       },
 
       redraw: function(){
         // A function to re-draw the table with same or modified values
       }
     }
+  }
+
+  // Generates test data with 'n' slices
+  ,generateTestData: function(n){
+    var data = [];
+    for(var x = 1; x <= n; x++){
+      data.push({
+        name: x.toString(),
+        value: x
+      });
+    }
+    return data;
   }
 });
